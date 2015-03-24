e7772963191fba622c8328ef8f9556600b09a131
Justine Ciullo <justinec1337@gmail.com>
2015-03-24 11:50:50 -0400

    added recipe class

---
+++ app/src/main/java/kaylamacfarlane/cuttingboard/Recipe.java
@@ -0,0 +1,26 @@
+package kaylamacfarlane.cuttingboard;
+
+import android.media.Image;
+
+/**
+ * Created by justine on 3/24/2015.
+ */
+public class Recipe
+{//start class
+
+    Image img;
+    String title;
+    String ingredients;
+    String cooktime;
+    String preptime;
+    String instructions;
+    String description;
+    String author;
+    int rating;
+
+
+    public void save()
+    {
+
+    }
+}//end class
